eee53fe8d40b1b2756fb9f961d10a947157e1247
Anup Dahal <adahal39@gmail.com>
2024-06-26 13:12:29 +0545

    auto complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import Autocompleteplaces from "../components/Autocompleteplaces";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div
+      className="bg-cover bg-center h-screen flex justify-center items-center"
+      style={{ backgroundImage: `url('vite.svg')` }}
+    >
+      <form className="flex">
+        <Autocompleteplaces />
+        <button className="bg-[#3d3880] hover:bg-[#3d3880] text-white font-bold py-2 px-4 ml-2 rounded-sm">
+          Continue
+        </button>
+      </form>
+    </div>
+  );
 }
