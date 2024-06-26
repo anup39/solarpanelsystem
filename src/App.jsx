45055077ba94fa39ac20f2b1094dd546654e6032
Anup Dahal <adahal39@gmail.com>
2024-06-26 10:15:04 +0545

    fixed the store and reducers

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+import Routers from "./routes/Routers";
+
+function App() {
+  return <Routers />;
 }
+
+export default App;
