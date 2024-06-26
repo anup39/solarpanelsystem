8b2cd459c33f3540603f95a110e293e34eaf6097
Anup Dahal <adahal39@gmail.com>
2024-06-26 10:02:10 +0545

    added tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,3 @@
-import { useState } from "react";
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>This is the map page</div>
-    </>
-  );
+export default function App() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
-
-export default App;
