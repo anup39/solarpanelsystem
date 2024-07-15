4ae565fe3c74035eb9ead248cfab9c9708b82a6b
Anup Dahal <adahal39@gmail.com>
2024-07-15 18:53:24 +0545

    infowindow

--- src/pages/MapSection.jsx
+++ src/pages/MapSection.jsx
@@ -6,6 +6,8 @@ import PropTypes from "prop-types";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
 import { useState } from "react";
+import Alert from "@mui/material/Alert";
+import Info from "@mui/icons-material/Info";
 
 const zoom = 21;
 const center = {
@@ -39,33 +41,35 @@ export default function MapSection({ loaded, map, onLoaded, onMap }) {
         />
       )}
 
-      <div className="absolute flex flex-row top-2 right-7 bg-[#3D3880] rounded-lg">
-        <FormControlLabel
-          onChange={(e) => {
-            if (currentMarker) {
-              // Ensure the marker reference exists
-              currentMarker.setVisible(e.target.checked); // Set visibility based on checkbox
-              setShowMarker(e.target.checked); // Update state
-            }
-          }}
-          sx={{
-            paddingLeft: "5px",
-            paddingRight: "5px",
-            color: "white",
-          }}
-          control={
-            <Checkbox
-              sx={{
-                color: "white",
-                "&.Mui-checked": {
+      <div className="absolute flex flex-row top-2 right-7 bg-[#3D3880] rounded-lg gap-32">
+        <div>
+          <FormControlLabel
+            onChange={(e) => {
+              if (currentMarker) {
+                // Ensure the marker reference exists
+                currentMarker.setVisible(e.target.checked); // Set visibility based on checkbox
+                setShowMarker(e.target.checked); // Update state
+              }
+            }}
+            sx={{
+              paddingLeft: "5px",
+              paddingRight: "5px",
+              color: "white",
+            }}
+            control={
+              <Checkbox
+                sx={{
                   color: "white",
-                },
-              }}
-              checked={showmarker}
-            />
-          }
-          label="Show Marker"
-        />
+                  "&.Mui-checked": {
+                    color: "white",
+                  },
+                }}
+                checked={showmarker}
+              />
+            }
+            label="Show Marker"
+          />
+        </div>
       </div>
 
       <div
@@ -84,6 +88,17 @@ export default function MapSection({ loaded, map, onLoaded, onMap }) {
           <h1>Panel Count:</h1>
         </div>
         <SliderSizes max={50} />
+      </div>
+
+      <div
+        style={{
+          display: currentMarker ? "block" : "none",
+        }}
+        className="absolute top-[1%] left-[40%]"
+      >
+        <Alert icon={<Info fontSize="inherit" />} severity="success">
+          Here move the marker to the desired location
+        </Alert>
       </div>
     </div>
   );
